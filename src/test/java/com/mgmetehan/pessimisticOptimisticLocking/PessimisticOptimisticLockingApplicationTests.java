b12307e5d622a4bfa5f54f21affd394e5eb51030
mgmetehan <mgmetehan@hotmail.com>
2024-10-29 21:21:54 +0300

    init project

---
+++ src/test/java/com/mgmetehan/pessimisticOptimisticLocking/PessimisticOptimisticLockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.pessimisticOptimisticLocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessimisticOptimisticLockingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
